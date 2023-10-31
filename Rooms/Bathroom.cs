9fda12c575d5404fcb64f89c2d951be327f49ac1
William Sylvestre <farakwarts@gmail.com>
2023-10-31 08:48:55 -0400

    Bain et miroir

--- Rooms/Bathroom.cs
+++ Rooms/Bathroom.cs
@@ -2,6 +2,7 @@
 {
     internal class Bathroom : Room
     {
+        internal static bool isHotWaterTrigged;
         internal override string CreateDescription() =>
 @"Dans la toilette, le [bain] est rempli d'eau chaude.
 Le [miroir] devant toi affiche ton visage déprimé.
@@ -13,10 +14,18 @@ Tu peux revenir dans ta [chambre].
             switch (choice)
             {
                 case "bain":
-                    Console.WriteLine("Tu te laisses relaxer dans le bain.");
+                    Console.WriteLine("Tu te laisses relaxer dans le bain. L'eau chaude remplie la pièce de vapeur... Quelque chose semble être écrit sur le miroir.");
+                    isHotWaterTrigged = true;
                     break;
                 case "miroir":
-                    Console.WriteLine("Tu aperçois les chiffres 2314 écrits sur la brume sur le miroir.");
+                    if (isHotWaterTrigged /*== true*/)
+                    {
+                        Console.WriteLine("Tu aperçois les chiffres 2314 écrits sur la brume sur le miroir.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Te voir aussi épuisé te donne envie de prendre un bon bain chaud.");
+                    }
                     break;
                 case "chambre":
                     Console.WriteLine("Tu retournes dans ta chambre.");
