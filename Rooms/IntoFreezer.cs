badea4149818d38856a185f406088409fc766b41
William Sylvestre <farakwarts@gmail.com>
2023-11-07 16:48:26 -0500

    Ajout de IntoFreezer & de Mama Olga

---
+++ Rooms/IntoFreezer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Rooms
+{
+    internal class IntoFreezer : Room
+    {
+        int getFrozen;
+        string warning;
+        internal override string CreateDescription() =>
+@"";
+
+        internal override void ReceiveChoice(string choice)
+        {
+            if (getFrozen >= 0 && getFrozen < 3)
+            {
+                warning = "";
+            }
+            else if (getFrozen >= 3 && getFrozen < 5)
+            {
+                warning =
+@"
+Il commence à faire vraiment froid, il vaudrait mieux sortir avant qu'il soit trop tard.";
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine(
+@"");
+                Game.Finish();
+            }
+
+            switch (choice)
+            {
+                case "cadavre":
+                    Console.WriteLine(
+@"" + warning);
+                    getFrozen++;
+                    break;
+
+                case "sac de munition":
+                    Console.WriteLine(
+@"" + warning);
+                    getFrozen++;
+                    break;
+
+                case "porte gelée":
+                    Console.WriteLine(
+@"" + warning);
+                    getFrozen++;
+                    break;
+
+                case "porte":
+                    Game.Transition<Freezer>();
+                    getFrozen = 0;
+                    break;
+                
+                default:
+                    Console.WriteLine("Commande invalide.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Rooms/MamaOlgaIsHere.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Rooms
+{
+    internal class MamaOlgaIsHere : Room
+    {
+        internal override string CreateDescription() =>
+@"
+Vous prenez la pièce. Mais au même moment, une secousse fait trambler le sol, la 
+poussière tombe du plafond et le son de moteur s'arrête. De grandes lumières jaunâtres
+se mettent à s'allumer, vous aveuglant au même moment.
+
+Vous êtes en panique ! Qu'est-ce qui se passe ?!
+
+Vous tenter de regardez les alentours maintenant que toute est éclairer. Et vous
+réalisez avec peur que cette endroit et gigatesque. Les étagères montent à presque 
+20 mètres de haut et un autre bon 8 mètres doit les séparer du plafond.
+
+« Mais où je suis ?... »
+
+Alors qu'un grand sentiment d'angoisse vous sumerge, vous entendez la porte dans le coin
+souvrir l'entement. Quelquechose arrive !
+
+Vous n'avez pas beaucoup de temps ! Vous pouvez vous [CACHER] ou [COURIR].
+";
+
+        internal override void ReceiveChoice(string choice)
+        {
+            switch (choice)
+            {
+                case "cacher":
+                    Console.Clear();
+                    Console.WriteLine(
+@"Vous décidez de vous cachez sous la caisse.
+Vous entendez le méchanisme des portes s'arrêter, suivit par le son
+de lourd pas.
+");
+                    Console.WriteLine(
+@"Le bruit se raproche de vous. Vous mettez vos mains sur votre
+bouche et votre nez pour couvrir votre respiration.
+");
+                    Console.WriteLine(
+@"La chose s'arrête juste devant vous, vous permettant de voir
+sa botte avec le bout recouvert d'une plaque de métal avec
+des pointes ensanglantées. Juste un pied suffirait à vous
+nécrasez la moitié du corps.
+");
+                    Console.WriteLine(
+@"Vous pouvez l'entendre faire une grognement étouffé... Puis
+continue son chemain. Vous laissant appercevoir le cadavre
+d'une créature humanoïde dont la moitier de la tête lu manque.
+Laissant derrière une énorme tracé de sang noir.
+");
+                    Console.WriteLine(
+@"
+Vous prennez quelque minute pour digérez tout ce qui vient de
+ce passer.
+
+« Qu'est-ce qui vient de ce passer? »
+« Suis-je en enfer? »
+« Comment sortir d'ici? »
+");
+                    Console.WriteLine(
+@"Vous comprennez que cette endroit est dangeureux et que
+vous devez vite trouvez un moyen de vous échaper.
+Vous regardez la pièce dans votre main et décidez de la
+garder. Elle vous sera peut-être utile.
+
+[PIÈCE] rajouté à votre inventaire.
+");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Game.Transition<Cashier>();
+                    break;
+
+                case "courir":
+                    Console.Clear();
+                    Console.WriteLine(
+@"Dans la panique, vous décidez de courir !
+
+Il ne faut pas beaucoup de temps à la créature avant de vous
+remarquez. Vous entendez derière vous un bruit de chaines qui
+s'entre choque. 
+
+Et alors que vous allez pour faire un virage et semer le
+monstre, vous entendez un *CLING!* et tombez au sol avec une
+grande douleur au niveau de votre jambe droite.
+");
+                    Console.WriteLine(
+@"Vous regardez lentement, avec hésitation, derière vous. Vous
+réaliser avec terreur qu'une machoire de métal, ressemblant à
+un piège à ours rouillé, rattachée à une chaine a pris au piège
+la totalité de votre jambe.
+");
+                    Console.WriteLine(
+@"Vous commencez à suivre la chaine des yeux et, vous la voyez.
+Une massive créature ressemblant à une femme corpulante dont
+la chaire rosée a été recousue et brûlée à certain endroit.
+
+Elle porte une large tablier vert couvert de sang. Une
+ceinture comportant divers outils de bouché et d'autres pièges.
+Tous liés par des chaines s'entremellant autour de ses bras
+et de ses cheville. 
+
+Des dizaine de longs piques tordue de métal rouillé semble
+avoir été plantés dans ses épaules. Ses poignets porte une
+épaise plaque de métal épineux cloué directement à sa chaire
+et, autour de son cou, vous pouvez remarquer trois crâne humain
+attacher à une corde
+");
+
+                    Console.WriteLine(
+@"Vous finissez par remarquer son visage, ou du moins le masque
+de métale qui le recouvre. Ses cheveux blanc et raides forme un
+amas de pointes, la rendant plus intimidant.
+");
+                    Console.WriteLine(
+@"
+Vous êtes soudainement tiré !
+
+La douleur est insuportable et vous ne pouvez vous empécher de
+crier et appeler à l'aide.
+
+Vous êtes trainé jusqu'au pied de la créature. Elle prend un
+moment de vous observez alors que vous essayez dans un effort
+désepéré de libérer votre jambe du piège, mais la prise est
+trop forte.
+");
+
+                    Console.WriteLine(
+@"Le monstre prend un grand hachoire accroché à sa ceinture et
+lève son bras en l'air.
+
+La dernière chose que vous voyez, est le hachoire s'enfonçant
+dans votre tête.
+
+VOUS ÊTES MORT
+");
+                    Game.Finish();
+                    break;
+
+                default:
+                    Console.WriteLine("Commande invalide.");
+                    break;
+            }
+        }
+    }
+}
