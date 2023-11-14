9c5f35b35ae020ef3d31ee27439c6a8aa804c92b
William Sylvestre <farakwarts@gmail.com>
2023-11-14 09:24:53 -0500

    fix bug in Phone

--- Rooms/Phone.cs
+++ Rooms/Phone.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace ProjetNarratif.Rooms
 {
@@ -49,10 +49,10 @@ moitié des boutons sont absents.");
 
                     if (Cashier.isCoinCollected == true)
                     {
-                    if (!alreadyTalk)
-                    {
-                        Console.WriteLine(
-@"
+                        if (!alreadyTalk)
+                        {
+                            Console.WriteLine(
+    @"
 Vous y insérez la pièce et prenez le combiné...
 
 - « Allo ?! »
@@ -70,35 +70,35 @@ RÉPONSES :
 2- Qui êtes-vous ?
 
 3- ...");
-                        choseQuestionAnswer = Convert.ToInt32(Console.ReadLine());
+                            choseQuestionAnswer = Convert.ToInt32(Console.ReadLine());
 
-                        Console.Clear();
-                        switch (choseQuestionAnswer)
-                        {
-                            case 1:
-                                Console.WriteLine(
-@"- « Bonjour petit, je suis BudZ ! J'imagine que tu as beaucoup de questions, mais ne t'inquiètes
+                            Console.Clear();
+                            switch (choseQuestionAnswer)
+                            {
+                                case 1:
+                                    Console.WriteLine(
+    @"- « Bonjour petit, je suis BudZ ! J'imagine que tu as beaucoup de questions, mais ne t'inquiètes
   pas, je suis ici pour t'aider. »");
-                                break;
+                                    break;
 
-                            case 2:
-                                Console.WriteLine(
-@"- « Les 12 soient loués ! Je n'ai pas vraiment de nom, mais on m'appelle BudZ et je suis ici
+                                case 2:
+                                    Console.WriteLine(
+    @"- « Les 12 soient loués ! Je n'ai pas vraiment de nom, mais on m'appelle BudZ et je suis ici
   pour aider les gens comme toi qui finissent par se retrouver coincés dans ce genre d'endroit. »");
-                                break;
+                                    break;
 
-                            default:
-                                Console.WriteLine(
-@"- « J'imagine que tu dois avoir peur. Je comprends parfaitement. Cet endroit doit être assez
+                                default:
+                                    Console.WriteLine(
+    @"- « J'imagine que tu dois avoir peur. Je comprends parfaitement. Cet endroit doit être assez
   disturbant de ton point de vue. Je sais aussi que d'entendre une voix à travers un téléphone
   qui ne devrait même plus marcher est tout aussi dérangeant. Mais ne t'en fais pas !
 
   Je suis Budz, et je suis là pour te sortir d'ici. »");
-                                break;
-                        }
+                                    break;
+                            }
 
-                        Console.WriteLine(
-@"
+                            Console.WriteLine(
+    @"
 -------------------------------------------
 RÉPONSES :
 
@@ -107,41 +107,41 @@ RÉPONSES :
 2- Où suis-je ?
 
 3- ...");
-                        choseQuestionAnswer = Convert.ToInt32(Console.ReadLine());
+                            choseQuestionAnswer = Convert.ToInt32(Console.ReadLine());
 
-                        Console.Clear();
-                        switch (choseQuestionAnswer)
-                        {
-                            case 1:
-                                Console.WriteLine(
-    @"- « Enchanté Hugo ! J'imagine que tu dois te demander où tu es. Malheureusement, tu n'es
+                            Console.Clear();
+                            switch (choseQuestionAnswer)
+                            {
+                                case 1:
+                                    Console.WriteLine(
+        @"- « Enchanté Hugo ! J'imagine que tu dois te demander où tu es. Malheureusement, tu n'es
   pas au meilleur endroit. Tu te trouves chez Mama Olga, ou plus précisément dans son magasin.
 
   La bonne nouvelle, c'est qu'elle est moins à l'affût lorsqu'elle est dans son magasin. Tu
   as donc une chance de t'en sortir... J'en ai déjà trop vu mourir, tu ne finiras pas comme
   eux. »");
-                                break;
+                                    break;
 
-                            case 2:
-                                Console.WriteLine(
-    @"- « Tu es malheureusement chez l'une des plus dangereuses habitantes des Limbes : Mama Olga.
+                                case 2:
+                                    Console.WriteLine(
+        @"- « Tu es malheureusement chez l'une des plus dangereuses habitantes des Limbes : Mama Olga.
   Mais ne t'en fais pas petit ! Si tu suis mes conseils, tu t'en sortiras vivant... J'en ai déjà
   trop vu mourir. »");
-                                break;
+                                    break;
 
-                            default:
-                                Console.WriteLine(
-    @"- « Je comprends, difficile de faire confiance dans un endroit pareil. Mais sans mon aide,
+                                default:
+                                    Console.WriteLine(
+        @"- « Je comprends, difficile de faire confiance dans un endroit pareil. Mais sans mon aide,
   Mama Olga te transformera en nourriture comme les produits que tu peux voir autour de toi !
   Alors s'il te plaît, écoute ce que j'ai à te dire... J'en ai déjà trop vu mourir, je ne veux
   pas que tu finisses comme eux. »");
-                                break;
-                        }
+                                    break;
+                            }
 
-                        do
-                        {
-                            Console.WriteLine(
-@"
+                            do
+                            {
+                                Console.WriteLine(
+    @"
 -------------------------------------------
 QUESTIONS :
 
@@ -156,30 +156,30 @@ QUESTIONS :
 5- À qui appartient ce journal ?
 
 6- J'y vais !");
-                            choseQuestionAnswer = Convert.ToInt32(Console.ReadLine());
+                                choseQuestionAnswer = Convert.ToInt32(Console.ReadLine());
 
-                            Console.Clear();
-                            switch (choseQuestionAnswer)
-                            {
-                                case 1:
-                                    Console.WriteLine(
-@"- « Pour te sortir d'ici, il faut que tu te rendes dans l'entrepôt du magasin. Tu pourras y
+                                Console.Clear();
+                                switch (choseQuestionAnswer)
+                                {
+                                    case 1:
+                                        Console.WriteLine(
+    @"- « Pour te sortir d'ici, il faut que tu te rendes dans l'entrepôt du magasin. Tu pourras y
   accèder en passant par le congélateur. Mais il te faudra avant tout la clé pour le cadenas qui
   maintient la porte de l'entrepôt scellé. Elle devrait se trouver sur le trousseau de clés dans
   la boucherie.
 
-  Une fois arrivé en arrière, il devrait avoir une trap qui sert à délivrer les charges. Je vais
+  Une fois arrivé en arrière, il devrait avoir une trappe qui sert à délivrer les charges. Je vais
   envoyer Hame - une bonne amie à moi -, pour venir te recueillir et t'amener en lieu sûr.
 
   J'ai failli oublier ! Mama Olga a ses propres chiens de garde. Il faudra que tu trouves un moyen
   de les occuper s'ils venaient à te barrer la route.
 
   As-tu d'autres questions ? »");
-                                    break;
+                                        break;
 
-                                case 2:
-                                    Console.WriteLine(
-@"- « Je ne la connais pas vraiment personnellement, mais je sais qu'elle est une des plus
+                                    case 2:
+                                        Console.WriteLine(
+    @"- « Je ne la connais pas vraiment personnellement, mais je sais qu'elle est une des plus
   dangereuses personnes de tous les Limbes. Elle était humaine avant, mais j'ignore ce qui a bien
   pu lui arriver pour qu'elle finisse ainsi. Cet endroit peut faire ressortir ce qu'il a de plus
   mauvais en nous... et parfois le meilleur.
@@ -191,11 +191,11 @@ QUESTIONS :
   ... J'ai l'impression que je viens de te donner un gros coup de stress sans le vouloir...
 
   As-tu d'autres questions ? »");
-                                    break;
+                                        break;
 
-                                case 3:
-                                    Console.WriteLine(
-@"- « Je suis ici depuis bien plus longtemps que tu pourrais le croire. Tu n'es pas le premier à
+                                    case 3:
+                                        Console.WriteLine(
+    @"- « Je suis ici depuis bien plus longtemps que tu pourrais le croire. Tu n'es pas le premier à
   arriver ici, et il y a d'autres qui sont dans des situations bien pire que la tienne. Parfois, il
   est possible de les sauver, mais la majeure partie du temps, je ne peux que les accompagner dans
   leur dernier instant... ça fait mal.
@@ -204,11 +204,11 @@ QUESTIONS :
   sortes vivant !
 
   As-tu d'autres questions ? »");
-                                    break;
+                                        break;
 
-                                case 4:
-                                    Console.WriteLine(
-@"- « Ça arrive de temps en temps que des humains ou des animaux finissent par arriver dans les
+                                    case 4:
+                                        Console.WriteLine(
+    @"- « Ça arrive de temps en temps que des humains ou des animaux finissent par arriver dans les
   Limbes, surtout depuis le Grand Buffet - crois-moi, tu ne veux pas que je te raconte cette
   histoire.
 
@@ -221,37 +221,37 @@ QUESTIONS :
   Ça, je n'en sais rien.
 
   As-tu d'autres questions ? »");
-                                    break;
+                                        break;
 
-                                case 5:
-                                    Console.WriteLine(
-@"- « Un journal ?... Ho ! Le journal ! Si c'est bien ce que je pense, garde le précieusement, il
+                                    case 5:
+                                        Console.WriteLine(
+    @"- « Un journal ?... Ho ! Le journal ! Si c'est bien ce que je pense, garde le précieusement, il
   t'aidera !
 
   Il appartenait à un ami. Il l'avait volontairement laissé au cas où quelqu'un finirait par tomber
   ici... Il a finalement eu raison de le faire.
 
   As-tu d'autres questions ? »");
-                                    break;
+                                        break;
 
-                                default:
-                                    break;
-                            }
-                        } while (choseQuestionAnswer < 5);
-                        Console.WriteLine(
-@"- « Très bien ! Si tu as besoin de moi, utilise ce téléphone et je te répondrais aussitôt !
+                                    default:
+                                        break;
+                                }
+                            } while (choseQuestionAnswer <= 5);
+                            Console.WriteLine(
+    @"- « Très bien ! Si tu as besoin de moi, utilise ce téléphone et je te répondrais aussitôt !
 
   Fais attention à toi ! »
 
 Le sou ressort de la machine et vous la récupérez.");
-                        specialEvent++;
-                        alreadyTalk = true;
-                    }
-                    else
-                    {
-                        string newVoice = voice[rdm.Next(voice.Length)];
-                        Console.WriteLine(
-@"
+                            specialEvent++;
+                            alreadyTalk = true;
+                        }
+                        else
+                        {
+                            string newVoice = voice[rdm.Next(voice.Length)];
+                            Console.WriteLine(
+    @"
 Vous déposez la pièce dans la fente et décrochez le téléphone.
 
 - « Bon retour ! Tu t'en sors toujours ? »
@@ -261,11 +261,10 @@ vous entendez sonne comme celle " + newVoice + @".
 
 - « Avais-tu d'autre questions ? »");
 
-                        choseQuestionAnswer = 0;
 
-                        do
-                        {
-                            Console.WriteLine(
+                            do
+                            {
+                                Console.WriteLine(
 @"
 -------------------------------------------
 QUESTIONS :
@@ -283,39 +282,41 @@ QUESTIONS :
 6- À qui appartient ce journal ?
 
 7- J'y vais !");
-                            choseQuestionAnswer = Convert.ToInt32(Console.ReadLine());
-                            switch (choseQuestionAnswer)
-                            {
-                                case 1:
-                                    Console.WriteLine(
-@"- « J'imagine que tu fais référence à ma voix, mais oui, c'est bien moi.
+                                choseQuestionAnswer = Convert.ToInt32(Console.ReadLine());
+
+                                Console.Clear();
+                                switch (choseQuestionAnswer)
+                                {
+                                    case 1:
+                                        Console.WriteLine(
+    @"- « J'imagine que tu fais référence à ma voix, mais oui, c'est bien moi.
 
   Pour une raison que j'ignore, lorsque quelqu'un démarre une nouvelle conversation avec moi, il aura
   l'impression de parler à une toute nouvelle personne. Ce qui est étrange puisque je suis persuadé
   d'avoir la même voix que d'habitude... Tu vas t'y faire.
 
   As-tu d'autres questions ? »");
-                                    break;
+                                        break;
 
-                                case 2:
-                                    Console.WriteLine(
-@"- « Pour te sortir d'ici, il faut que tu te rendes dans l'entrepôt du magasin. Tu pourras y
+                                    case 2:
+                                        Console.WriteLine(
+    @"- « Pour te sortir d'ici, il faut que tu te rendes dans l'entrepôt du magasin. Tu pourras y
   accèder en passant par le congélateur. Mais il te faudra avant tout la clé pour le cadenas qui
   maintient la porte de l'entrepôt scellé. Elle devrait se trouver sur le trousseau de clés dans
   la boucherie.
 
-  Une fois arrivé en arrière, il devrait avoir une trap qui sert à délivrer les charges. Je vais
+  Une fois arrivé en arrière, il devrait avoir une trappe qui sert à délivrer les charges. Je vais
   envoyer Hame - une bonne amie à moi -, pour venir te recueillir et t'amener en lieu sûr.
 
   J'ai failli oublier ! Mama Olga a ses propres chiens de garde. Il faudra que tu trouves un moyen
   de les occuper s'ils venaient à te barrer la route.
 
   As-tu d'autres questions ? »");
-                                    break;
+                                        break;
 
-                                case 3:
-                                    Console.WriteLine(
-@"- « Je ne la connais pas vraiment personnellement, mais je sais qu'elle est une des plus
+                                    case 3:
+                                        Console.WriteLine(
+    @"- « Je ne la connais pas vraiment personnellement, mais je sais qu'elle est une des plus
   dangereuses personnes de tous les Limbes. Elle était humaine avant, mais j'ignore ce qui a bien
   pu lui arriver pour qu'elle finisse ainsi. Cet endroit peut faire ressortir ce qu'il a de plus
   mauvais en nous... et parfois le meilleur.
@@ -327,11 +328,11 @@ QUESTIONS :
   ... J'ai l'impression que je viens de te donner un gros coup de stress sans le vouloir...
 
   As-tu d'autres questions ? »");
-                                    break;
+                                        break;
 
-                                case 4:
-                                    Console.WriteLine(
-@"- « Je suis ici depuis bien plus longtemps que tu pourrais le croire. Tu n'es pas le premier à
+                                    case 4:
+                                        Console.WriteLine(
+    @"- « Je suis ici depuis bien plus longtemps que tu pourrais le croire. Tu n'es pas le premier à
   arriver ici, et il y a d'autres qui sont dans des situations bien pire que la tienne. Parfois, il
   est possible de les sauver, mais la majeure partie du temps, je ne peux que les accompagner dans
   leur dernier instant... ça fait mal.
@@ -340,11 +341,11 @@ QUESTIONS :
   sortes vivant !
 
   As-tu d'autres questions ? »");
-                                    break;
+                                        break;
 
-                                case 5:
-                                    Console.WriteLine(
-@"- « Ça arrive de temps en temps que des humains ou des animaux finissent par arriver dans les
+                                    case 5:
+                                        Console.WriteLine(
+    @"- « Ça arrive de temps en temps que des humains ou des animaux finissent par arriver dans les
   Limbes, surtout depuis le Grand Buffet - crois-moi, tu ne veux pas que je te raconte cette
   histoire.
 
@@ -357,32 +358,32 @@ QUESTIONS :
   Ça, je n'en sais rien.
 
   As-tu d'autres questions ? »");
-                                    break;
+                                        break;
 
-                                case 6:
-                                    Console.WriteLine(
-@"- « Un journal ?... Ho ! Le journal ! Si c'est bien ce que je pense, garde le précieusement, il
+                                    case 6:
+                                        Console.WriteLine(
+    @"- « Un journal ?... Ho ! Le journal ! Si c'est bien ce que je pense, garde le précieusement, il
   t'aidera !
 
   Il appartenait à un ami. Il l'avait volontairement laissé au cas où quelqu'un finirait par tomber
   ici... Il a finalement eu raison de le faire.
 
   As-tu d'autres questions ? »");
-                                    break;
+                                        break;
 
-                                default:
-                                    break;
-                            }
-
-                            Console.Clear();
-                        } while (choseQuestionAnswer < 6);
-                        Console.WriteLine(
+                                    default:
+                                        break;
+                                }
+                            } while (choseQuestionAnswer <= 6);
+                            Console.WriteLine(
 @"- « Très bien ! Reste sur tes garde et fais attention à toi ! »
 
 Le sou ressort de la machine et vous la récupérez.");
-                        specialEvent++;
+                            specialEvent++;
+                            alreadyTalk = true;
+                        }
+                        
                     }
-                }
                     break;
 
                 case "détritus":
@@ -421,7 +422,7 @@ mais vous arrivez à lire ce qui suit :
 ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
 09/24/83
 
-Une semaine s'est écoulée depuis la disparition des sept adolescents,
+Une semaine s'est écoulée depuis la disparition des quatre adolescents,
 les familles, des amis et les gens du quartier continuent de porter des
 fleurs devant le vieux magasin abandonné.
 
