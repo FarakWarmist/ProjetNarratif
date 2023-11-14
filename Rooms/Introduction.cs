5ff711bf078a6558253e017d0be8bb05c9880a48
William Sylvestre <farakwarts@gmail.com>
2023-11-14 09:24:36 -0500

    temporary update for Introduction (need new description)

--- Rooms/Introduction.cs
+++ Rooms/Introduction.cs
@@ -4,20 +4,7 @@
     {
         internal override string CreateDescription() =>
 @"
-Un étrange épouventail se tient au loin devant un vieux arbre mort noir comme le charbon,
-au millieu d'un champ de pierre tombale qui sétant jusqu'à l'horizon.
-derrière l'arbre, une énorme éclipse solaire dans un ciel rouge ourangé vous donne
-le sentiment d'un œil qui vous observe jusqu'au tréfond de votre âme.
-
-        
-
-L'épouventail lève lentement la tête pour vous regarder. Malgrés la distance qui vous séparre,
-vous pouvez l'entendre vous chuchotter...
-
-« Réveille... toi... »
-
-Et vous reprenez connaissance.
-";
+Vous vous réveillez dans une étrange endroit....";
 
         internal override void ReceiveChoice(string choice)
         {
