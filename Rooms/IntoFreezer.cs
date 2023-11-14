6d4cdaf6931c0ed4d91763d85be13fea36b0c186
William Sylvestre <farakwarts@gmail.com>
2023-11-14 09:25:32 -0500

    add descriptions and choices in IntoFreezer

--- Rooms/IntoFreezer.cs
+++ Rooms/IntoFreezer.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +11,24 @@ namespace ProjetNarratif.Rooms
         int getFrozen;
         string ? warning;
 
+        internal static bool freezingDoorUnlock;
         internal static bool isLeadBulletsCollected;
+        internal static bool isMessageCollected;
+        internal static string ? toDo;
 
         internal override string CreateDescription() =>
-@"";
+@"
+Vous ouvrez la grande porte coulissante et êtes acceuilli par une terible vent glacé. Vous
+savez que restez dans cette chambre froide quelques minutes suffira à vous paralyser de
+froid.
+
+Vous pouvez voir des table en métale dont une avec un [CORPS] dessus. Des morceaux de partie
+humaine, et d'autre non-humain, sont suspendu au plafond par des crochets.Au fond de la
+pièce, vous appercevez une [PORTE GELÉE] avec un gros [CADENAS] qui le maintient fermée. 
+
+Vous pouvez [REGARDER] les alentours, regarder votre [INVENTAIRE] ou vous diriger vers un autre
+endroit avec votre [CARTE].
+";
 
         internal override void ReceiveChoice(string choice)
         {
@@ -26,41 +40,177 @@ namespace ProjetNarratif.Rooms
             {
                 warning =
 @"
+
 Il commence à faire vraiment froid, il vaudrait mieux sortir avant qu'il soit trop tard.";
             }
             else
             {
                 Console.Clear();
                 Console.WriteLine(
-@"");
+@"Vous ne sentez plus vos mains.
+Vos jambes refuse de bouger.
+Vous pouvez voir vos yeux se cristalisé.
+
+Le froid a eu le dessus sur vous.
+
+VOUS ÊTES MORT
+");
                 Game.Finish();
             }
 
             switch (choice)
             {
-                case "cadavre":
-                    Console.WriteLine(
-@"" + warning);
+                case "corps":
                     getFrozen++;
+                    Console.WriteLine(
+@"Vous allez vers le corps.
+
+Le cadavre est étandus dos à la table et n'est vétu que d'un linge qui couvre le bas de
+son corps. Son torse a été soigneusement ouvert et est totalement creux.Une épaisse
+fante sépare presque en deux sa tête de haut en bas. Un de ses bras semble en mauvais état,
+ressemblant à un crayon qu'on aurait trop machouillé.");
+
+                    if (!isLeadBulletsCollected)
+                    {
+                        Console.WriteLine(
+@"
+Dans l'autre main, il semble tenir quelquechoes fermemant
+
+Voulez-vous voir ce que c'est ?
+[OUI] | [NON]
+");
+                        toDo = Console.ReadLine().ToLower();
+                        if (toDo == "oui")
+                        {
+                            Console.WriteLine(
+@"Vous entrouvrez sa main pour voir un sac avec à l'intérieur des [BALLES EN PLOMB]
+ressemblant à des billes en métal." + warning);
+                        }
+                        else
+                        {
+                            Console.WriteLine(
+@"Vous ne vous s'entez pas super bien de toucher un mort. Vous laissez tombez et vous vous
+éloignez du corps." + warning);
+                        }
+                    }
+
                     break;
 
-                case "sac de munition":
-                    Console.WriteLine(
-@"" + warning);
+                case "balles en plomb":
                     getFrozen++;
+                    if (!isLeadBulletsCollected)
+                    {
+                        Console.WriteLine(
+@"Vous prenez le sac que tenait le cadavre et le m'êtez dans votre poche" + warning +
+@"
+[BALLES EN PLOMB] rajoutées à votre inventaire.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+@"Un petit sac remplie de balles en plombs.");
+                    }
+
                     break;
 
                 case "porte gelée":
-                    Console.WriteLine(
-@"" + warning);
+                    if (!Butchery.isBunchOfKeysCollected)
+                    {
+                        getFrozen++;
+                        Console.WriteLine(
+@"Vous insérez l'une des clés du trousseau dans le cadenas et arrivez à le débarrer.
+
+La [PORTE GELÉE] est maintenant déverouillée." + warning);
+                        freezingDoorUnlock = true;
+                    }
+
+                    if (!freezingDoorUnlock)
+                    {
+                        getFrozen++;
+                        Console.WriteLine(
+@"La porte complètement gelée est vérouillé par un gros cadenas. Un hublot se trouve sur le
+haut de la porte, hors de votre portée. Vous pourrier prendre une [CAISSE] à votre portée pour
+voir ce qu'il y a de l'autre côté." + warning);
+                        
+                    }
+                    else
+                    {
+                        if (!BackstoreWithDogs.friendlyDogs)
+                        {
+                            Game.Transition<BackstoreWithDogs>();
+                        }
+                        else
+                        {
+                            Game.Transition<Backstore>();
+                        }
+                    }
+                    break;
+
+                case "caisse":
                     getFrozen++;
+                    Console.WriteLine(
+@"Vous rapprochez une caisse près de la porte et regarder entravers du hublot. Malgré le givre
+qui recouvre la vitre, vous pouvez voir un couloir qui va de gauche à droite dont les murs sont
+recouvert de tuyaux et d'engrenages.
+En regardant plus bas, vous voyez ce que vous penssez être 2 chiens qui se reposent et gardent
+l'entrée.
+Ils seront certainement un problème lorsque vous ouvrirez la porte.");
                     break;
 
                 case "porte":
+                    Console.WriteLine(
+@"Vous ressortez de la chambre froide.
+La chaleur du magazin vous fait un bien fou après avoir été dans ce froid mortel.");
                     Game.Transition<Freezer>();
                     getFrozen = 0;
                     break;
-                
+
+                case "regarder":
+                    getFrozen++;
+                    Console.WriteLine(
+@"En regardant ce que contient la chambre froide, vous pouvez voir au pied de la table où est
+couché le [CADAVRE] des [VÊTEMENTS] qui semble hors du commun.");
+                    break;
+
+                case "vêtements":
+                    getFrozen++;
+                    Console.WriteLine(
+@"Vous vous raprochez du linge et remarquer qu'il ressemble à une tenue que vous avez vu dans
+vous cours d'histoire. Ces vêtements sont exactement comme les uniformes que portait les soldats
+britaniques lors du 18ème siècle. Comment s'est arrivé ici ?
+
+En soulevant la tunique, un morceau de papier tombe d'une des poche. Un [MESSAGE] semble être
+écrit dessus.");
+                    break;
+
+                case "message":
+                    getFrozen++;
+                    Console.WriteLine(
+@"Le message est difficile à lire, mais vous arrivez à lire une partie :
+
+« Que dieu me pardonne pour cette pensé, mais le champ boueux et le sifflement des balles de
+la guerre commence à me manquer. Le son scilencieux de moteur et les corps terrifiés sont pire
+que l'enfer.
+
+J'ai vu la chose qui vit ici. Elle est pire que tout les horreurs décrit par la bible. Elle se
+promène avec ces créature, des chiens difforme et démoniaque. Ils sembles la craindre toute
+autant que moi. Ils semblent affamé ces pauvres bêtes.
+
+Qu'est-ce que je racconte ? Me voilà entrain de prendre en pitié ces démons.
+
+Je ne sais pas depuis combien de temps je suis ici, ni combien de temps ça va duré. J'ai du me
+nourrir de ce qu'offrait ce magasin pour survivre, et je brulerait probablement en enfer après
+avoir su ce que je mangais.
+
+Parfois, je t'entend m'appelé maman. Je pense que cette endroit me rend fou. Ou est-ce la mort
+qui m'appelle.
+
+Tu me manques maman, j'aurais voulu m'excusé avant de partir pour la bataille. J'aimerais te
+dire que je suis déso... »
+
+Le reste du texte est taché par le sang.");
+                    break;
+
                 case "journal":
                     Game.Journal();
                     break;
