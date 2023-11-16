c98b35c0cced7814dd9aa1b50c7d039ab774c972
William Sylvestre <farakwarts@gmail.com>
2023-11-16 10:51:25 -0500

    add "mécanique de jeu" in Introduction

--- Rooms/Introduction.cs
+++ Rooms/Introduction.cs
@@ -17,7 +17,21 @@ parler dans d'autres jeux).
 Vous revivez les souvenirs d'Hugo Weston, un jeune ado de 14 ans qui se retrouve
 coincé dans l'épicerie et de terrible et terrifiante de Mama Olga, appeler le « Meat
 Store ». Vous devrez fouiller et trouver l'équipement nécessaire pour votre invasion
-et votre survie.";
+et votre survie.
+
+- Mécaniques de jeu -
+
+Les mots encadrés de [CROCHETS] signifient que vous pouvez interagir avec, y effectuer
+une action ou vous y déplacer.
+
+Vous pouvez à tout moment utiliser votre Inventaire pour regarder tout ce que vous
+avez collecté jusqu'à maintenant. Tout objet collecté peut être utilisé à n'importe
+quel moment, mais ça ne veut pas dire que ça marchera tout le temps.
+
+Une fois collecté, vous pourrez vous référer au Journal pour avoir des indices sur
+ce qu'il faut que vous fassiez et comment survivre à certains problèmes que vous
+devrez faire face. Vous collecterez aussi une Carte qui vous permettra de vous
+déplacer dans les différentes zones du magasin.";
 
         internal override void ReceiveChoice(string choice)
         {
