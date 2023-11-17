5d4de46aefd85bc2c8f0521a4e80ce8e15654d07
William Sylvestre <farakwarts@gmail.com>
2023-11-17 15:42:17 -0500

    fix spelling errors in BackstoreWithDogs

--- Rooms/BackstoreWithDogs.cs
+++ Rooms/BackstoreWithDogs.cs
@@ -12,13 +12,16 @@ namespace ProjetNarratif.Rooms
         internal static string? toDo;
 
         internal override string CreateDescription() =>
-@"Vous arrivez face à 2 créature canine. Leurs yeux, ainsi que cetaine partie de leur corps,
-sont couvert par des plaques de métal cloué directement à leur chaire. Leur machoir sont plus
-long, leurs de dents plus nombreux et leur masse plus imposant que des chiens normaux. Mais
-vous pouvez pas vous empêcher de remarquer qu'ils ont l'aire affamé et mal nourrie.
+@"Vous arrivez face à 2 créatures canines. 
 
-Vous n'avez pas beaucoup de choix maintenant. Vous pouvez essayer des [COURIR] ou de
-regardez votre [INVENTAIRE] si quelque chose pourrait vous aidez.";
+Leurs yeux, ainsi que certaines parties de leur corps, sont couverts par des
+plaques de métal cloué directement à leur chaire. Leur mâchoire est plus longue,
+leur nombre de dents plus nombreux et leur masse plus imposante que des chiens
+normaux. Mais vous ne pouvez pas vous empêcher de remarquer qu'ils ont l'air
+affamés et mal nourris.
+
+Vous n'avez pas beaucoup de choix maintenant. Vous pouvez essayer de [COURIR]
+ou de regarder dans votre [INVENTAIRE] si quelque chose pourrait vous aider.";
 
         internal override void ReceiveChoice(string choice)
         {
@@ -27,20 +30,24 @@ regardez votre [INVENTAIRE] si quelque chose pourrait vous aidez.";
                 case "courir":
                     Console.Clear();
                     Console.WriteLine(
-@"Vous tentez de vous sauvez, mais à la seconde où vous vous appraîtez à vous retourner,
-vous sentez vos jambes et bras se déchiqueter par les machoires de ses monstres.
+@"Vous tentez de vous sauver, mais à la seconde où vous vous apprêtez à vous
+retourner, vous sentez soudainement vos jambes et bras se déchiqueter par les
+mâchoires de ces monstres.
 
-Vous êtes secouez dans tout les senses et vous débattre et complètement futile.
+Vous êtes secoué dans tous les sens et vous débattre est complètement futile.
 
-Alors que vous ressentez votre corps disparaître à chaque bouchez que prennent ces
-chiens, vous réalisez que vous allez mourir... Si prêt du but...
+Alors que vous ressentez votre corps disparaître à chaque bouchée prise par les
+chiens, vous réalisez que c'est la fin pour vous... 
+
+... Si prêt du but...
 
 
 VOUS ÊTES MORT !
 
 ___________________________
 Voulez-vous revenir en arrière ?
-[OUI] | [NON]");
+[OUI] | [NON]
+");
 
                             toDo = Console.ReadLine().ToLower();
                             if(toDo == "non")
@@ -62,15 +69,16 @@ Voulez-vous revenir en arrière ?
                     {
                         Console.Clear();
                         Console.WriteLine(
-@"Vous sortez de votre sac le tuyau en métal prit précédement.
+@"Vous sortez de votre sac le tuyau en métal prit précédemment.
 
-Vous vous mettez en garde et instatanément un des chiens vous saute dessus avant que vous
-puissiez porter le premier coup. Vous tombez sur le dos, maintenant le tuyau entre vous et
-la geule qui essait de vous dévorer. Mais en quelque machouillement, vous êtes surpris de
-voir que le chien a réussit à gruger le tuyau en deux.
+Vous vous mettez en garde et instantanément un des chiens vous saute dessus
+avant que vous puissiez porter le premier coup. Vous tombez sur le dos, maintenant
+le tuyau entre vous et la gueule qui essaie de vous dévorer. Mais en quelques
+mâchouillements, vous êtes surpris de voir que le chien a réussi à gruger le tuyau
+en deux.
 
-Mais la surprise ne dur pas longtemps, lorsque vous réalisez que vous êtes à l'intérieu d'un
-machoire prêt à croquer.
+Mais la surprise ne dur pas longtemps, lorsque vous réalisez que votre tête est à
+l'intérieur d'une mâchoire prête à croquer.
 
 
 VOUS ÊTES MORT !
@@ -99,20 +107,22 @@ Voulez-vous revenir en arrière ?
                     else
                     {
                         Console.WriteLine(
-@"Vous sortez lentement, sans mouvement brusque, le sac de viande que vous avez ramassez
-précédement.
+@"Vous sortez lentement, sans mouvements brusques, le sac de viande que vous avez
+ramassé précédemment.
 
-Les chiens passent d'un état aggressif, à curieux, puis enjoué à fur et à mesure qu'il
-réalise ce qu'il y a dans le sac.
+Les chiens passent d'un état agressif, à curieux, puis enjoué à fur et à mesure
+qu'ils réalisent ce qu'il y a dans le sac. Lorsque vous sortez le premier morceau,
+les deux s'assirent immédiatement, leur queue montrant leur excitation.
 
-Lorsque vous sortez le premier morceau, les deux se sassirent immédiatement, leur queue
-montrant leur exitation. Malgrés la situation, vous pouvez vous empêcher de sourir de
-joie face à ces deux monstres canidés qui agissent soudainement comme deux chiens
-attendant sagement leur gaterie.
+Malgré la situation, vous ne pouvez vous empêcher de sourire à la vue de ces deux
+monstres canidés qui agissent soudainement comme deux bons chiens attendant sagement
+leur gâterie.
 
-Vous lancez les morceaux de viande un a un, avec à chaque fois un des deux monstres qui
-finissait par l'attraper en plaine air. Après avoir bien mangée, les deux paraît
-rasasier et moins enclin à vous sauter à la gorge.");
+Vous lancez les morceaux de viande un à un, chaque fois attraper en plein air par un
+des deux chiens.
+
+Après avoir bien mangé, les deux paraient rassasiés et moins enclins à vous sauter à
+la gorge.");
                         friendlyDogs = true;
                         Freezer.isMeatCollected = false;
                         Game.Transition<Backstore>();
